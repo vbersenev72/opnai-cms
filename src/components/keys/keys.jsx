c4f9af366d7fe1c35f39654badc05d5407ec6882
Vladik <vbersenev72@gmail.com>
2023-05-13 12:34:29 +0500

    axios

--- src/components/keys/keys.jsx
+++ src/components/keys/keys.jsx
@@ -1,17 +1,40 @@
 import * as React from 'react';
 
-
 import './keys.css'
+import axios from 'axios';
 
 
 
 
 const Keys = () => {
 
+    const getKeys = async () => {
+        const key1 = await axios.get('http://localhost:5000/keys/1')
+        const key2 = await axios.get('http://localhost:5000/keys/2')
+        console.log(key1)
+        console.log(key2)
+    }
+
+    React.useEffect(() => {
+        getKeys()
+    }, [])
+
 
     return (
         <div>
-        sdfd
+            <div className="keys_container">
+
+                <div className="key">
+                    <h1>Ключ 1</h1> <br/>
+                    контейнер 1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, eum, cum error laudantium expedita nemo repellendus illo sapiente aspernatur modi, quasi itaque consequuntur? Corrupti amet suscipit velit neque facilis atque?
+                </div>
+
+                <div className="key">
+                    <h1>Ключ 2</h1> <br/>
+                    контейнер 2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, eum, cum error laudantium expedita nemo repellendus illo sapiente aspernatur modi, quasi itaque consequuntur? Corrupti amet suscipit velit neque facilis atque?
+                </div>
+
+            </div>
         </div>
     )
 }
--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,12 +1,16 @@
-
+import './menu.css'
 
 
 const MenuBar = () => {
 
   return (
-    <div className='menu_container'>
-
-    </div>
+    <header className='menu_container'>
+      <div className='menu_links'>
+        <nav className='link'><a href="/">Ключи OpenAI</a></nav>
+        <nav className='link'><a href="/server">Ошибки</a></nav>
+        <nav className='link'><a href="/database">База данных</a></nav>
+      </div>
+    </header>
   );
 };
 export default MenuBar;
