bea7fedd614e6dd614638e236ab5f8c351525041
Vladik <vbersenev72@gmail.com>
2023-06-16 17:25:34 +0500

    add

--- src/components/requests_filter/requests_filter.jsx
+++ src/components/requests_filter/requests_filter.jsx
@@ -8,13 +8,16 @@ import React from 'react';
 const Requests_filter = () => {
 
   const [data, setData] = useState([])
+  const [errors, setErrors] = useState([])
+
   const [StartDate, setStartDate] = React.useState()
   const [EndDate, setEndDate] = React.useState()
 
-  const [listDate, setListDate] = React.useState([]) 
+  const [onlySubscribed, setOnlySubscribed] = React.useState(false)
+  const [subscribed, setSubscribed] = React.useState([])
 
 
- 
+
 
   const getData = async (startDate, endDate) => {
     console.log(startDate);
@@ -23,26 +26,71 @@ const Requests_filter = () => {
       start_date: startDate,
       end_date: endDate
     })
+
+    const responseErrors = await axios.post('http://77.105.136.213:5000/keys/getbydate_errors', {
+      start_date: startDate,
+      end_date: endDate
+    })
     setData(response.data.message)
-    console.log(data);
-    
+    console.log(responseErrors.data);
+    data.map((obj)=>{
+      if (!obj.is_subscribed) {
+        setSubscribed([...subscribed, obj])
+      }
+    })
+
+    setErrors(responseErrors.data.message)
+
+
   }
-  var uniqueDates = [...new Set(data.map((obj) => obj.date.substring(0, 10)))];
+  let uniqueDates = [...new Set(data.map((obj) => obj.date.substring(0, 10)
+  ))]
   uniqueDates.sort((a, b) => {
     if (a < b) {
-      return 1
-    } else {
       return 0
+    } else {
+      return 1
     }
   })
-  
 
 
-  const tableData = uniqueDates.map((obj) => {
+  const tableData = uniqueDates.map((date) => {
+    if (!onlySubscribed) {
+      const filteredObjects = data.filter((obj) => obj.date.substring(0, 10) === date);
+      const queries = filteredObjects.map((obj) => obj.data)
+      const responses = filteredObjects.map((obj) => obj.response);
+      const totalChars = filteredObjects.reduce((acc, obj) => acc + obj?.data?.length, 0);
+      const totalResponseChars = filteredObjects.reduce((acc, obj) => acc + obj?.response?.length, 0);
+      const errs = errors.filter((err) => err.date.substring(0, 10) == date)
+      return {
+        date,
+        queries,
+        responses,
+        totalChars,
+        totalResponseChars,
+        errs
+      };
+
+
+
+    } else {
+      const filteredObjects = subscribed.filter((obj) => obj.date.substring(0, 10) === date);
+      const queries = filteredObjects.map((obj) => obj.data)
+      const responses = filteredObjects.map((obj) => obj.response);
+      const totalChars = filteredObjects.reduce((acc, obj) => acc + obj?.data?.length, 0);
+      const totalResponseChars = filteredObjects.reduce((acc, obj) => acc + obj?.response?.length, 0);
+      const errs = errors.filter((err) => err.date.substring(0, 10) == date)
+      return {
+        date,
+        queries,
+        responses,
+        totalChars,
+        totalResponseChars,
+        errs
+      };
+    }
 
   })
-
-
 
 
 
@@ -52,10 +100,9 @@ const Requests_filter = () => {
         selected={StartDate}
         onChange={(val)=>{
           setStartDate(new Date(val.getTime()))
-          console.log(StartDate);
         }}
 
-        placeholderText={'Выберите начальную дату'} 
+        placeholderText={'Выберите начальную дату'}
       />
       <h3>До</h3>
         <DatePicker
@@ -63,8 +110,14 @@ const Requests_filter = () => {
         onChange={(val) => {
           setEndDate(new Date(val.getTime()))
         }}
-        placeholderText={'Выберите конечную дату'} 
+        placeholderText={'Выберите конечную дату'}
       />
+
+      <div style={{display: 'flex'}}>
+        <input type="checkbox" placeholder='Только платные' checked={onlySubscribed} onChange={()=>setOnlySubscribed(!onlySubscribed)} />
+        <h4>Только платные</h4>
+      </div>
+
       <br />
       <br />
       <br />
@@ -75,20 +128,20 @@ const Requests_filter = () => {
               <th>Дата</th>
               <th>Запросы</th>
               <th>Ответы</th>
-              <th>Количество символов</th>
+              <th>Количество символов (запрос)</th>
               <th>Количество символов (ответ)</th>
               <th>Ошибки</th>
             </tr>
           </thead>
           <tbody>
-            {uniqueDates.map((item, index) => (
+            {tableData.map((item, index) => (
               <tr key={index}>
-                <td>{item}</td>
-                {/* <td>{item.queries}</td>
-                <td>{item.responses}</td>
-                <td>{item.characterCount}</td>
-                <td>{item.responseCharacterCount}</td>
-                <td>{item.errors}</td> */}
+                <td>{item.date}</td>
+                <td>{(item.queries.length)}</td>
+                <td>{(item.responses.length)}</td>
+                <td>{(((item.totalChars)/item.queries.length))=='NaN'? (item.totalChars)/item.queries.length : item.totalResponseChars/item.responses.length*0.07 }</td>
+                <td>{(item.totalResponseChars)/item.responses.length}</td>
+                <td>{item.errs.length}</td>
               </tr>
               ))}
           </tbody>
